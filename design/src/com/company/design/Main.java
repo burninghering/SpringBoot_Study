c95ea79500719156a4f2246af64739bc24984f94
김혜린 <37091602+burninghering@users.noreply.github.com>
2022-02-20 19:40:34 +0900

    Proxy pattern

--- design/src/com/company/design/Main.java
+++ design/src/com/company/design/Main.java
@@ -1,6 +1,9 @@
 package com.company.design;
 
 import com.company.design.adapter.*;
+import com.company.design.proxy.Browser;
+import com.company.design.proxy.BrowserProxy;
+import com.company.design.proxy.IBrowser;
 import com.company.design.singleton.Aclazz;
 import com.company.design.singleton.Bclazz;
 import com.company.design.singleton.SocketClient;
@@ -8,7 +11,6 @@ import com.company.design.singleton.SocketClient;
 public class Main {
 
     public static void main(String[] args) {
-
         //singleton start->
 //        Aclazz aclazz = new Aclazz();
 //        Bclazz bclazz = new Bclazz();
@@ -19,16 +21,29 @@ public class Main {
         //<-end singleton
 
         //adapter start->
-        HairDryer hairDryer = new HairDryer();
-        connect(hairDryer);
+//        HairDryer hairDryer = new HairDryer();
+//        connect(hairDryer);
+//
+//        Cleaner cleaner = new Cleaner();
+//        Electronic110V adapter = new SocketAdapter(cleaner); //Electronic110V 인터페이스 타입을 가진 SocketAdapter의 인스턴스
+//        connect(adapter);
+//
+//        AirConditioner airConditioner = new AirConditioner();
+//        Electronic110V airAdater = new SocketAdapter(airConditioner);
+//        connect(airAdater);
+        //<-end adapter
+        //proxy start->
+//        Browser browser = new Browser("www.naver.com");
+//        browser.show();
+//        browser.show();
+//        browser.show();
+//        browser.show(); // 매번 네이버로부터 url을 받아오고 있음
 
-        Cleaner cleaner = new Cleaner();
-        Electronic110V adapter = new SocketAdapter(cleaner); //Electronic110V 인터페이스 타입을 가진 SocketAdapter의 인스턴스
-        connect(adapter);
-
-        AirConditioner airConditioner = new AirConditioner();
-        Electronic110V airAdater = new SocketAdapter(airConditioner);
-        connect(airAdater);
+        IBrowser browser = new BrowserProxy("www.naver.com");
+        browser.show(); //처음에는 url을 네이버에서 받아오지만
+        browser.show(); //캐시 기능 사용해 불러오고 있다.
+        browser.show();
+        browser.show();
     }
 
     //adapter : 콘센트
