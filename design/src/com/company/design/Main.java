098abe1fef03163765d4320381293d005b9aed2d
김혜린 <37091602+burninghering@users.noreply.github.com>
2022-03-08 03:52:47 +0900

    Observer pattern

--- design/src/com/company/design/Main.java
+++ design/src/com/company/design/Main.java
@@ -3,6 +3,8 @@ package com.company.design;
 import com.company.design.adapter.*;
 import com.company.design.aop.AopBrowser;
 import com.company.design.decorator.*;
+import com.company.design.observer.Button;
+import com.company.design.observer.IButtonListener;
 import com.company.design.proxy.Browser;
 import com.company.design.proxy.BrowserProxy;
 import com.company.design.proxy.IBrowser;
@@ -50,18 +52,35 @@ public class Main {
 //        browser.show();
         //<-end proxy
 
-        ICar audi=new Audi(1000);
-        audi.showPrice();
+        //Decorator start->
+//        ICar audi=new Audi(1000);
+//        audi.showPrice();
+//
+//        //a3
+//        ICar audi3 = new A3(audi,"A3");
+//        audi3.showPrice();
+//        //a4
+//        ICar audi4 = new A4(audi,"A4");
+//        audi4.showPrice();
+//        //a5
+//        ICar audi5 = new A5(audi,"A5");
+//        audi5.showPrice();
+        //<-end Decorator
 
-        //a3
-        ICar audi3 = new A3(audi,"A3");
-        audi3.showPrice();
-        //a4
-        ICar audi4 = new A4(audi,"A4");
-        audi4.showPrice();
-        //a5
-        ICar audi5 = new A5(audi,"A5");
-        audi5.showPrice();
+        Button button = new Button("버튼");
+
+        button.addListener(new IButtonListener() {
+            @Override
+            public void clickEvent(String event) {
+                System.out.println(event);
+            }
+        });
+
+        button.click("메시지 전달 : click 1");
+        button.click("메시지 전달 : click 2");
+        button.click("메시지 전달 : click 3");
+        button.click("메시지 전달 : click 4");
+
     }
 
     //adapter : 콘센트
