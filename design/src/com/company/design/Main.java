87cda887e1ad353f053d072839fe4a4d130dc3fe
김혜린 <37091602+burninghering@users.noreply.github.com>
2022-03-08 22:39:59 +0900

    Strategy pattern

--- design/src/com/company/design/Main.java
+++ design/src/com/company/design/Main.java
@@ -15,6 +15,7 @@ import com.company.design.proxy.IBrowser;
 import com.company.design.singleton.Aclazz;
 import com.company.design.singleton.Bclazz;
 import com.company.design.singleton.SocketClient;
+import com.company.design.strategy.*;
 
 import static java.awt.PageAttributes.MediaType.A4;
 
@@ -87,33 +88,56 @@ public class Main {
 //        button.click("메시지 전달 : click 4");
         //<-end Observer
 
-        Ftp ftpClient = new Ftp("www.foo.co.kr",22,"/home/etc");
-        ftpClient.connect();
-        ftpClient.moveDirectory();
+//        Ftp ftpClient = new Ftp("www.foo.co.kr",22,"/home/etc");
+//        ftpClient.connect();
+//        ftpClient.moveDirectory();
+//
+//        Writer writer = new Writer("text.tmp");
+//        writer.fileConnect();
+//        writer.write();
+//
+//        Reader reader = new Reader("text.tmp");
+//        reader.fileConnect();
+//        reader.fileRead();
+//
+//        reader.fileDisconnect();
+//        writer.fileDisconnect();
+//        ftpClient.disConnect();
+//        //위 예시는 굉장히 추상화된 형태이지만, 각각의 객체에 너무 의존하고 있다.
+//
+//        //그러므로 아래와 같이 파사드 패턴을 적용시키면,
+//        //앞 쪽 정면만 바라볼 수 있도록 객체 하나 만들 수 있고,
+//        //여러가지 의존성을 가진 것을 새로운 인터페이스 형태로 제공한다.
+//        SftpClient sftpClient = new SftpClient("www.foo.co.kr",22,"/home/etc","text.tmp");
+//        sftpClient.connect();
+//        sftpClient.write();
+//        sftpClient.read();
+//        sftpClient.disConnect();
 
-        Writer writer = new Writer("text.tmp");
-        writer.fileConnect();
-        writer.write();
+        Encoder encoder = new Encoder(); //전략을 사용하기 위한 원본 객체(변하지 않음)
 
-        Reader reader = new Reader("text.tmp");
-        reader.fileConnect();
-        reader.fileRead();
+        //base64
+        EncodingStrategy base64 = new Base64Strategy(); //전략 생성
 
-        reader.fileDisconnect();
-        writer.fileDisconnect();
-        ftpClient.disConnect();
-        //위 예시는 굉장히 추상화된 형태이지만, 각각의 객체에 너무 의존하고 있다.
+        //normal
+        EncodingStrategy normal = new NomalStrategy(); //전략 생성
 
-        //그러므로 아래와 같이 파사드 패턴을 적용시키면,
-        //앞 쪽 정면만 바라볼 수 있도록 객체 하나 만들 수 있고,
-        //여러가지 의존성을 가진 것을 새로운 인터페이스 형태로 제공한다.
-        SftpClient sftpClient = new SftpClient("www.foo.co.kr",22,"/home/etc","text.tmp");
-        sftpClient.connect();
-        sftpClient.write();
-        sftpClient.read();
-        sftpClient.disConnect();
+
+        String message="Hello java";
+
+        encoder.setEncodingStrategy(base64); //전략 사용할 때마다 세팅
+        String base64Result = encoder.getMessage(message);
+        System.out.println(base64Result);
+
+        encoder.setEncodingStrategy(normal);
+        String normalResult = encoder.getMessage(message);
+        System.out.println(normalResult);
+
+        encoder.setEncodingStrategy(new AppendStrategy());
+        String appendResult = encoder.getMessage(message);
+        System.out.println(appendResult);
+        //전략을 주입함으로써 결과가 달라지도록 함함
     }
-
     //adapter : 콘센트
     public static void connect(Electronic110V electronic110V){
         electronic110V.powerOn();
