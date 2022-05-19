ebf138453d2f413fbfe9d0c04ea3d4acf03f4533
김혜린 <pearlyn@naver.com>
2022-05-20 04:09:27 +0900

    DI

---
+++ demo/ioc/src/com/company/ioc/Base64Encoder.java
@@ -0,0 +1,10 @@
+package com.company.ioc;
+
+import java.util.Base64;
+
+public class Base64Encoder implements IEncoder{
+
+    public String encode(String message){ //메세지를 하나 받아서
+        return Base64.getEncoder().encodeToString(message.getBytes()); //메세지를 getBytes로 리턴
+    }
+}
---
+++ demo/ioc/src/com/company/ioc/Encoder.java
@@ -0,0 +1,14 @@
+package com.company.ioc;
+
+public class Encoder {
+
+    private IEncoder iEncoder; //외부에서 주입받음 -> Dependancy Injection을 받은 것이다
+
+    public Encoder(IEncoder iEncoder){
+        this.iEncoder=iEncoder;
+    }
+
+    public String encode(String message){
+        return iEncoder.encode(message);
+    }
+}
